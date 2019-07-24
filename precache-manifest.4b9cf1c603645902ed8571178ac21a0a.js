b0157c03203100a44a177c80fb23c8a88a8a1f9b
Iryna <iriskag@ukr.net>
2019-07-24 17:44:36 +0300

    Updates

---
+++ precache-manifest.4b9cf1c603645902ed8571178ac21a0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fdf503f1931ab030e0ae2561e7b2a9f",
+    "url": "/react_carousel/index.html"
+  },
+  {
+    "revision": "3524231d79b627d7212b",
+    "url": "/react_carousel/static/css/main.12ef6e3b.chunk.css"
+  },
+  {
+    "revision": "774d5e3722e17f42d013",
+    "url": "/react_carousel/static/js/2.cba05053.chunk.js"
+  },
+  {
+    "revision": "3524231d79b627d7212b",
+    "url": "/react_carousel/static/js/main.85069d65.chunk.js"
+  },
+  {
+    "revision": "a62ff4406f9253df9292",
+    "url": "/react_carousel/static/js/runtime~main.30902e3a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_carousel/precache-manifest.50801b1752512efc4ec66a2e23e808f4.js"
+  "/react_carousel/precache-manifest.4b9cf1c603645902ed8571178ac21a0a.js"
 );
 
 self.addEventListener('message', (event) => {
